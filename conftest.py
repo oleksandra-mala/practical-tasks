4531067c31809c288e15bbaa03e3f988288e4b0b
Oleksandra Mala <aldorokhina@gmail.com>
2026-02-26 11:34:21 +0100

    update fixture

--- conftest.py
+++ conftest.py
@@ -1,43 +1,19 @@
 import pytest
 import requests
-from flask import session
 
 
 
 @pytest.fixture(scope="session")
 def base_url():
-    return "https://reqres.in/api/users?page=1"
+    return "https://reqres.in/api"
 
-@pytest.fixture
-def api_client():
+
+@pytest.fixture(scope="session")
+def api_client(base_url):
     session = requests.Session()
-
+    session.headers.update({
+        "x-api-key": 'pub_90e18fe41c093dff780bafe29a201ae015c5068c10fc8f108285e2b94dd34727',
+        "content-type": "application/json"
+    })
     yield session
     session.close()
-
-@pytest.fixture
-def auth_token():
-    response = requests.post(
-        "https://reqres.in/api/login",
-        json={
-            "email": "eve.holt@reqres.in",
-            "password": "cityslicka"
-        }
-    )
-
-    print("STATUS:", response.status_code)
-    print("BODY:", response.text)
-
-    assert response.status_code == 200
-
-    return response.json()["token"]
-
-# @pytest.fixture
-# def api_client(base_url, auth_token):
-#     session = requests.Session()
-#     session.headers.update({
-#         "x-api-key": f"Bearer {auth_token}",
-#         "Content-Type": "application/json"
-#     })
-#     yield session
-#     session.close()
--- test_get.py
+++ test_get.py
@@ -2,6 +2,6 @@ def test_get_users(api_client, base_url):
     response = api_client.get(f"{base_url}/users/1")
     assert response.status_code == 200
     data = response.json()
-    assert data['data'][0]['id'] == 1
+    assert data['data']['id'] == 1
 
 
