3fa7558b1bdc1b21cf9dfbb2a32493c8380f3324
Oleksandra Mala <aldorokhina@gmail.com>
2026-02-26 12:16:56 +0100

    add second get request

---
+++ test_GET.py
@@ -0,0 +1,15 @@
+from conftest import api_client
+
+
+def test_get_users(api_client, base_url):
+    response = api_client.get(f"{base_url}/users/1")
+    assert response.status_code == 200
+    data = response.json()
+    assert data['data']['id'] == 1
+
+def test_get_user_by_id(api_client, base_url):
+    user_id = 1
+    response = api_client.get(f"{base_url}/users/{user_id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data['data']['first_name'] == 'George'
--- test_get.py
+++
@@ -1,7 +0,0 @@
-def test_get_users(api_client, base_url):
-    response = api_client.get(f"{base_url}/users/1")
-    assert response.status_code == 200
-    data = response.json()
-    assert data['data']['id'] == 1
-
-
