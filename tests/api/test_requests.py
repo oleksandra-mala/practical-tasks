67de0500a2ecbf8964df5532a6e53b58e9d2593d
Oleksandra Mala <aldorokhina@gmail.com>
2026-03-11 16:01:50 +0100

    files updated include GET

--- tests/api/test_requests.py
+++ tests/api/test_requests.py
@@ -5,11 +5,6 @@ def test_single_user(api_client, base_url):
     assert data['data']['id'] == 2
     assert data['data']['first_name'] == 'Janet'
 
-def test_list_users(api_client, base_url):
-    response = api_client.get(f"{base_url}/users")
-    assert response.status_code == 200
-    data = response.json()
-    assert data['data']['last_name'] == 'Wong'
 
 def test_get_user_by_id(api_client, base_url):
     user_id = 1
